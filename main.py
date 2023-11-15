3abd0ffdde097987de53532f6335762579f9dd55
Marat <marat.safarli@gmail.com>
2023-11-15 19:22:05 +0100

    added the plots

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
 import openpyxl
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 
 #import and basic cleaning
@@ -16,3 +19,31 @@ data.rename(columns={'Unnamed: 0': 'year',
                      'SP.POP.TOTL' : 'population',
                      'NV.AGR.TOTL.KD' :  'agricultural_GDP'     
                      }, inplace=True)
+data.set_index('year', inplace=True)
+
+
+for col in data.columns:
+    fig, axs = plt.subplots(3, 1, figsize=(12, 15)) 
+    axs[0].plot(data.index, data[col], label=col)
+    axs[0].set_xlabel('Year')
+    axs[0].set_ylabel('Level')
+    axs[0].legend()
+  
+    axs[1].plot(data.index, np.log(data[col]), label=col)
+    axs[1].set_xlabel('Year')
+    axs[1].set_ylabel('Log(Level)')
+    axs[1].legend()
+
+    axs[2].plot(data.index, data[col].diff(), label=col)
+    axs[2].set_xlabel('Year')
+    axs[2].set_ylabel('First Difference')
+    axs[2].legend()
+
+    output_directory = 'graphs'
+    os.makedirs(output_directory, exist_ok=True)
+    output_file_path = os.path.join(output_directory, f'{col}.png')
+    plt.savefig(output_file_path)
+
+    plt.show()
+
+    
