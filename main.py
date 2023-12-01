4081e56aad67ae331be569117d70d4f5a89d4485
Mooibroek <Robert.Mooibroek@rabobank.nl>
2023-12-01 17:01:09 +0100

    new variables

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.stattools import acf, pacf
 from statsmodels.tsa.stattools import adfuller, kpss, zivot_andrews
 from arch.unitroot import PhillipsPerron
+from arch.unitroot.cointegration import engle_granger, phillips_ouliaris, DynamicOLS, FullyModifiedOLS
 import statsmodels.api as sm
 from statsmodels.stats.stattools import durbin_watson
 from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.vector_ar.vecm import coint_johansen
+from statsmodels.tsa.vector_ar.vecm import coint_johansen, VECM, select_coint_rank
 from tabulate import tabulate
-from arch.unitroot import ADF, PhillipsPerron, KPSS, ZivotAndrews
+
 
 # Get and set the current working directory
 working_directory = os.path.dirname(__file__)
@@ -21,30 +22,31 @@ os.chdir(working_directory)
 #import and basic cleaning
 file_path=os.path.join(os.getcwd(), 'MVE_Assignment_DataSet.xlsx')
 columns = ['Unnamed: 0',
-           #'EN.ATM.METH.AG.KT.CE',
-           #'EN.ATM.NOXE.AG.KT.CE',
-           #'AG.PRD.CROP.XD',
-           #'EN.ATM.CO2E.KT',
+           'EN.ATM.METH.AG.KT.CE',
+           'EN.ATM.NOXE.AG.KT.CE',
+           'AG.PRD.CROP.XD',
+           'EN.ATM.CO2E.KT',
            'mean+tmp',
            'mean_pre',
            'SP.POP.TOTL',
-           'NV.AGR.TOTL.KD']
+           'NV.AGR.TOTL.KD'
+           ]
 
 data = pd.read_excel(file_path, header=1, usecols=columns)
 
 data.rename(columns={'Unnamed: 0': 'year',
                      #'EN.ATM.METH.AG.KT.CE' : 'methane' ,
                      #'EN.ATM.NOXE.AG.KT.CE' :  'nox' ,
-                     #'AG.PRD.CROP.XD' :  'crop_production' ,
-                     #'EN.ATM.CO2E.KT' :  'co2',
+                     'AG.PRD.CROP.XD' :  'crop_production' ,
+                     'EN.ATM.CO2E.KT' :  'co2',
                      'mean+tmp' :  'mean_temp',
                      'mean_pre': 'mean_rainfall',
-                     'SP.POP.TOTL' : 'population',
-                     'NV.AGR.TOTL.KD' :  'agricultural_GDP'     
+                     #'SP.POP.TOTL' : 'population',
+                     #'NV.AGR.TOTL.KD' :  'agricultural_GDP'     
                      }, inplace=True)
 data.set_index('year', inplace=True)
 
-data=data[['mean_temp','mean_rainfall','population','agricultural_GDP']]
+data=data[['crop_production', 'mean_temp','mean_rainfall','co2']]
 
 for col in data.columns:
     fig, axs = plt.subplots(3, 1, figsize=(12, 15)) 
@@ -70,8 +72,8 @@ for col in data.columns:
 
     plt.show()
 
-# Take the natural logarithm of population and agricultural GDP to stabilize variance and distribution
-data[['population', 'agricultural_GDP']] = np.log(data[['population', 'agricultural_GDP']])
+# Take the natural logarithm of CO2 to stabilize variance and distribution
+data[['co2']] = np.log(data[['co2']])
 
 
 #Part 3
@@ -171,6 +173,72 @@ for i in range(3):
     global_variable_name = f"df_result_diff{i}"
     globals()[global_variable_name] = unit_root_test(data, i).round(4)
     print(globals()[global_variable_name])
+
+
+# Part 4 Robert: Test for (no)-cointegration using some of the techniques discussed during the course
+
+# All possible cointegration relationships
+coint_relations = {
+                   'test 1':{'y': 'crop_production', 'x': ['mean_temp', 'mean_rainfall', 'co2']},
+                   'test 2':{'y': 'crop_production', 'x': ['mean_temp', 'mean_rainfall']},
+                   'test 3':{'y': 'crop_production', 'x': ['mean_rainfall','co2']},
+                   'test 4':{'y': 'crop_production', 'x': ['mean_temp', 'co2']},
+                   'test 5':{'y': 'crop_production', 'x': ['mean_temp']},
+                   'test 6':{'y': 'crop_production', 'x': ['mean_rainfall']},
+                   'test 7':{'y': 'crop_production', 'x': ['co2']}
+                   }
+                   
+
+# Defining function to test cointegration according to Engle-Granger, Phillips Ouliaris, and Johansen
+def cointegration_test(data:pd.DataFrame, y:str, x:list):
+    eg_coint_result = engle_granger(data[y], data[x])
+    po_coint_result = phillips_ouliaris(data[y], data[x], test_type="Zt")
+    print(f"Dependent variable: {y}")
+    print(f"Independent variable(s): {x}")
+    print()
+    
+    # Engle-Granger cointegration
+    print(f"The p-value of the Engle-Granger cointegration test is: {eg_coint_result.pvalue}")
+    if eg_coint_result.pvalue < 0.05:
+        print("The variables are cointegrated")
+    else:
+        print("The variables are not cointegrated")
+    print()
+    
+    # Phillips Ouliaris cointegration
+    print(f"The p-value of the Phillips Ouliaris cointegration test is: {po_coint_result.pvalue}")
+    if eg_coint_result.pvalue < 0.05:
+        print("The variables are cointegrated")
+    else:       
+        print("The variables are not cointegrated")
+    print()
+    
+    # Johansen Trace
+    johansen_df = pd.concat([data[y], data[x]], axis=1)
+    trace_coint_result = select_coint_rank(endog=johansen_df, det_order=0, k_ar_diff=1, method='trace')
+    maxeig_coint_result = select_coint_rank(endog=johansen_df, det_order=0, k_ar_diff=1, method='maxeig')
+    print(f"There are {trace_coint_result.rank} cointegrating vectors according to the Johensen Trace test")
+    print(f"There are {maxeig_coint_result.rank} cointegrating vectors according to the Johensen Maximum Eigenvalue test")
+    print()
+
+# Testing for cointegration
+for i in coint_relations:
+    cointegration_test(data, coint_relations[i]['y'], coint_relations[i]['x'])
+    print()
+
+
+# Define function for cointegrating regressions
+def cointegration_regression(data: pd.DataFrame, y:str, x:list):
+    static_ols = sm.OLS(data[y], sm.add_constant(data[x])).fit()
+    dynamic_ols = DynamicOLS(data[y], data[x], lags=1).fit()
+    fully_mod_ols = FullyModifiedOLS(data[y], data[x]).fit()
+
+    return static_ols.summary()
+
+
+cointegration_regression(data, coint_relations['test 1']['y'], coint_relations['test 1']['x'])
+
+
 
 
 #Part 4
